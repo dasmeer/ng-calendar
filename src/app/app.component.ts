345089e2f067209fba73abda614a7a822daca74a
dasmeer <qast@bk.ru>
2019-09-19 19:11:24 +0530

    current day

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
     years: Number[] = [];
     selectedYear: Number;
     disablingIsOn = false;
+    currentDayIsOn = true;
     ru = true;
     // holidays: string = '{"a":[{ "1": [ "1", "2", "3", "4", "5", "6", "7", "8" ]} , {"2": [ "23" ]}, {"3": [ "8" ]}, {"5": [ "1", "9" ]}, {"6": [ "12" ]}, {"11": [ "4" ] }]}';
 
